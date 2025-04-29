2f146e58c60d2f7562f665bc92fadf6b6fd2ec9c
BillohMBarry <alhajibillohbarrie@gamil.com>
2025-04-29 23:23:45 +0000

    add an input field for ingredient

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
- 
+  const [ingredient,setIngredient] = useState([""])
+  const addIngredient = (FormData) => {
+    const items = FormData.get("ingredients")
+    setIngredient(preIngredient => [...preIngredient, items])
+    console.log(items)
+  }
   return (
     <>
       <header>
         <h3>CHEF CLAUDE</h3>
       </header>
-      
+      <section>
+        <form action={addIngredient}>
+          <input type="text" placeholder='e.g onions' name="ingredients"  />
+          <button>Add Ingredient</button>
+        </form>
+        <h1>Ingredient on hand:</h1>
+        <ul>
+          <li>{ingredient}</li>
+        </ul>
+      </section>
    
     </>
   )
